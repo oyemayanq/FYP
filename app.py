5cebc3b09f84d547bad3a3132a6d828af45b0fc2
Mayank Mishra <maymishraank@gmail.com>
2023-04-05 17:11:04 +0530

    user input

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ def hello():
 @app.route("/result",methods=['POST'])
 def result():
     user_input = request.form["user_input"]
+
     result = ""
+
     if platform.system() == "Windows":
     	result = subprocess.run(['python','result.py'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     else:
